aa4912d8e2f1300ad64540084aa2caf3bbab9bd6
Srihari Srinivasan <118172807+speedwagon1299@users.noreply.github.com>
2025-01-07 17:03:46 +0000

    Codespace add

---
+++ myStatements.py
@@ -0,0 +1,13 @@
+my_list = ["apple", 
+            "cherry", 
+            "lemon"]
+my_dict = {
+    "drink": "coffee",
+    "milk": "whole"
+}
+
+for item in my_list:
+    print(f'My fav thing to eat is {item}')
+
+for key, val in my_dict.items():
+    print(f'My fav thing to drink is {val}')
